bf063efee44ec54b941da23cc7d8030f59269e47
krxzy37 <pala44archuk@gmail.com>
2025-12-24 13:24:26 +0200

    create wallet system

--- pkg/wallet.go
+++ pkg/wallet.go
@@ -1,1 +1,23 @@
 package pkg
+
+import "errors"
+
+type Waller interface {
+	Deposit(amount float64)
+	Withdraw(amount float64)
+	Balance() float64
+}
+type BitcoinWallet struct {
+	name    string
+	balance int
+}
+
+type UsdWallet struct {
+	name    string
+	balance int
+}
+
+var (
+	ErrAmountBelowZero   = errors.New("the sum must be greater than zero")
+	ErrInsufficientFunds = errors.New("insufficient funds")
+)
