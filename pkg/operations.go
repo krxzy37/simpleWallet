d2eec8b94e6177e8709bf2d20376b3f1b5e09d18
krxzy37 <pala44archuk@gmail.com>
2025-12-24 14:00:00 +0200

    create withdraw operation

--- pkg/operations.go
+++ pkg/operations.go
@@ -52,3 +52,41 @@ func (b BitcoinWallet) Deposit(amount float64) {
 
 	return
 }
+
+func (b BitcoinWallet) Withdraw(amount float64) {
+
+	var sAmount string
+	fmt.Println("Enter the deposit amount: ")
+	_, err := fmt.Scan(&sAmount)
+	if err != nil {
+		return
+	}
+
+	amount, err = strconv.ParseFloat(sAmount, 64)
+	if b.balance < amount {
+		fmt.Println("Error:", ErrAmountBelowZero)
+		return
+	}
+
+	b.balance -= amount
+
+}
+
+func (u UsdWallet) Withdraw(amount float64) {
+
+	var sAmount string
+	fmt.Println("Enter the deposit amount: ")
+	_, err := fmt.Scan(&sAmount)
+	if err != nil {
+		return
+	}
+
+	amount, err = strconv.ParseFloat(sAmount, 64)
+	if u.balance < amount {
+		fmt.Println("Error:", ErrAmountBelowZero)
+		return
+	}
+
+	u.balance -= amount
+
+}
